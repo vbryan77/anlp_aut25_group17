df12e5509982fa701de3f1a09368c938e96cf033
bryanbryan77 <71356626+bryanbryan77@users.noreply.github.com>
2025-04-23 20:13:11 +1000

    Initial commit

--- import streamlit as st.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import joblib
-from sklearn.metrics.pairwise import cosine_similarity
-
-vectorizer = joblib.load('vectorizer.pkl')
-resp_vecs = joblib.load('resp_vecs.pkl')
-df = pd.read_csv('df_match.csv')
-
